20f1ad813d08c0b09d39ed2fd2c8a7d8432f9493
Manas Paul <manas@codelogicx.com>
2021-05-14 11:51:34 +0530

    theme data learing

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.light,
+        darkTheme: ThemeData(brightness: Brightness.dark),
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
