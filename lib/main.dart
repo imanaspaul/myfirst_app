1c653f22985640fd0588e7cb8c559c9f516a6441
Manas Paul <manas@codelogicx.com>
2021-05-14 12:09:03 +0530

    Images aseets added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myfirst_app/screens/home_page.dart';
+import 'package:myfirst_app/screens/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +10,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        themeMode: ThemeMode.light,
-        darkTheme: ThemeData(brightness: Brightness.dark),
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: HomePage());
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/login": (context) => HomePage(),
+      },
+    );
   }
 }
